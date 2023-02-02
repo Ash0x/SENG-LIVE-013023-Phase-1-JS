132f5247f7c1250e198998ba2bac8b547f222228
Alexander <alex.galev@yahoo.com>
2023-02-02 11:58:06 -0700

    syncing

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -5,12 +5,46 @@ function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
 
+function renderHeader(bookStore) {
+  document.querySelector('#store-name').textContent = bookStore.name
+}
+renderHeader(bookStore)
+
+function renderFooter(bookStore) {
+document.querySelector('#location').textContent = `Location: ${bookStore.location}`
+document.querySelector('#number').textContent = `Phone Number: ${bookStore.number}`
+document.querySelector('#address').textContent = `Address: ${bookStore.address}`
+document.querySelector('#hours').textContent = `Hours of operation: ${bookStore.hours}`
+}
+
+renderFooter(bookStore)
 // create a function called renderBook(book)
 // it will take a book object as an argument
 // and create the html struture for rendering 
 // that book and insert it into our webpage!
 
-// function renderBook(book) {
+const bookList = document.querySelector('#book-list')
+function renderBook(book) {
+  const li = document.createElement('li')
+  li.className = 'list-li'
+
+  const h3 = document.createElement('h3')
+  h3.textContent = book.title
+  li.append(h3)
+  
+    const pAuthor = document.createElement('p')
+    pAuthor.textContent = book.author
+    li.append(pAuthor)
+
+  const pPrice = document.createElement('p')
+  pPrice.textContent = formatPrice(book.price)
+  li.append(pPrice)
+
+  bookList.append(li)
+  
+}
+const book = bookStore.inventory[0]
+renderBook(book)
 // should create an li element that looks something like this:
   // <li class="list-li">
   //   <h3>Eloquent JavaScript : A Modern Introduction to Programming</h3>
@@ -19,5 +53,3 @@ function formatPrice(price) {
   //   <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
   //   <button>Delete</button>
   // </li>
-
-  
--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -1,6 +1,6 @@
-const bookList = document.querySelector('#book-list');
+const bookList = document.querySelector('#book-list')
 function formatPrice(price) {
-  return '$' + Number.parseFloat(price).toFixed(2);
+	return '$' + Number.parseFloat(price).toFixed(2)
 }
 
 //////////////////////////////////////
@@ -9,14 +9,14 @@ function formatPrice(price) {
 
 // create a function renderHeader() that takes the store name from bookStore and adds to the DOM
 function renderHeader(bookStore) {
-  document.querySelector('#store-name').textContent = bookStore.name;
+	document.querySelector('#store-name').textContent = bookStore.name
 }
 
 function renderFooter(bookStore) {
-  document.querySelector('#location').textContent = bookStore.location;
-  document.querySelector('#number').textContent = bookStore.number;
-  document.querySelector('#address').textContent = bookStore.address;
-  document.querySelector('#hours').textContent = bookStore.hours;
+	document.querySelector('#location').textContent = bookStore.location
+	document.querySelector('#number').textContent = bookStore.number
+	document.querySelector('#address').textContent = bookStore.address
+	document.querySelector('#hours').textContent = bookStore.hours
 }
 
 // function: renderBook(book)
@@ -31,41 +31,84 @@ function renderFooter(bookStore) {
 // </li>
 // appends the li to the ul#book-list in the DOM
 function renderBook(book) {
-    
-  const li = document.createElement('li');
-  li.className = 'list-li';
-  
-  const h3 = document.createElement('h3');
-  h3.textContent = book.title;
-  li.append(h3);
+	const li = document.createElement('li')
+	li.className = 'list-li'
 
-  const pAuthor = document.createElement('p');
-  pAuthor.textContent = book.author;
-  li.append(pAuthor);
-  
-  const pPrice = document.createElement('p');
-  pPrice.textContent = formatPrice(book.price);
-  li.append(pPrice);
-  
-  const img = document.createElement('img');
-  img.src = book.imageUrl;
-  img.alt = `${book.title} cover`;
-  img.title = `${book.title} cover`;
-  li.append(img);
+	const h3 = document.createElement('h3')
+	h3.textContent = book.title
+	li.append(h3)
 
-  const btn = document.createElement('button');
-  btn.textContent = 'Delete';
-  li.append(btn);
+	const pAuthor = document.createElement('p')
+	pAuthor.textContent = book.author
+	li.append(pAuthor)
 
-  bookList.append(li);
+	const pPrice = document.createElement('p')
+	pPrice.textContent = formatPrice(book.price)
+	li.append(pPrice)
+
+	const img = document.createElement('img')
+	img.src = book.imageUrl
+	img.alt = `${book.title} cover`
+	img.title = `${book.title} cover`
+	li.append(img)
+
+	const btn = document.createElement('button')
+	btn.textContent = 'Delete'
+	btn.addEventListener('click', (e) => {
+		li.remove()
+	})
+	li.append(btn)
+
+	bookList.append(li)
 }
 
+function fillIn(form, data) {
+	form.title.value = data.title
+	form.author.value = data.author
+	form.price.value = data.price
+	form.imageUrl.value = data.imageUrl
+	form.inventory.value = data.inventory
+}
+
+const bookForm = document.querySelector('#book-form')
+
+fillIn(bookForm, {
+	title: 'Designing Data-Intensive applications',
+	author: 'Martin Kleppmann',
+	price: 22.22,
+	imageUrl:
+		'https://m.media-amazon.com/images/I/51ZSpMl1-LL._SX379_BO1,204,203,200_.jpg',
+	inventory: 1
+})
+
+bookForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+	const newBook = {
+		title: e.target.title.value,
+		author: e.target.author.value,
+		price: parseFloat(e.target.price.value),
+		reviews: [],
+		inventory: parseInt(e.target.inventory.value),
+		imageUrl: e.target.imageUrl.value
+	}
+	renderBook(newBook)
+})
+
+const toggleForm = document.querySelector('#toggleForm')
+
+toggleForm.addEventListener('click', (e) => {
+	const isHidden = bookForm.classList.toggle('collapsed')
+	if (isHidden) {
+		e.target.textContent = 'New Book'
+	} else {
+		e.target.textContent = 'Hide Book Form'
+	}
+})
 
 ////////////////////////////////////////////
 // call render functions to populate the DOM
 ////////////////////////////////////////////
 
-renderHeader(bookStore);
-renderFooter(bookStore);
-bookStore.inventory.forEach(renderBook);
-
+renderHeader(bookStore)
+renderFooter(bookStore)
+bookStore.inventory.forEach(renderBook)
